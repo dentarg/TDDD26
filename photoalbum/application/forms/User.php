c8dd0ea8fdc6566f9c862d7dbb9d6c5ff4d4f220
Patrik Ragnarsson <patrik@starkast.net>
2010-05-05 16:22:33 +0200

    Use input type="password"

--- photoalbum/application/forms/User.php
+++ photoalbum/application/forms/User.php
@@ -21,7 +21,7 @@ class Application_Form_User extends Zend_Form
             ->addValidator('NotEmpty');
 
 
-        $password = new Zend_Form_Element_Text('password');
+        $password = new Zend_Form_Element_Password('password');
         $password->setLabel('Password')
             ->setRequired(true)
             ->addFilter('StripTags')
