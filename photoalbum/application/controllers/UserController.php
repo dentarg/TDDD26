17d4c8604591af3d2e0f671d27763749aa92a7cf
Patrik Ragnarsson <patrik@starkast.net>
2010-05-07 15:17:15 +0200

    User login, untested stuff

--- photoalbum/application/controllers/UserController.php
+++ photoalbum/application/controllers/UserController.php
@@ -13,10 +13,10 @@ class UserController extends Zend_Controller_Action
 		//User list
     }
 
-    public function showAction()
+    public function showAction() 
     {
 		$userNamespace = new Zend_Session_Namespace('Zend_Auth'); //For debugging reasons...
-		$userNamespace->id = 1;
+		$userNamespace->id = 2;
 	
 		//If user is not logged in and he didn't specified id of the user he wants to view then he is redirected to
 		//index page. It's temp. redirect page can be changed in the future or it can throw an error
@@ -82,21 +82,83 @@ class UserController extends Zend_Controller_Action
 			} 
 			else 
 			{
-				$form->populate($formData);
+				//$form->populate($formData); // unnecessary
 			}
 		}		
 	}
 	
 	public function loginAction()
 	{
-		if(!isset($userNamespace) && $this->_postParam('login'))
+	   // from http://framework.zend.com/manual/en/zend.db.adapter.html
+      $db = new Zend_Db_Adapter_Pdo_Mysql(array(
+         'host'     => '127.0.0.1',
+         'username' => 'tddd26',
+         'password' => '123123',
+         'dbname'   => 'tddd26'));
+
+	   // from http://framework.zend.com/manual/en/zend.auth.adapter.dbtable.html
+      $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
+      $authAdapter
+         ->setTableName('user')
+         ->setIdentityColumn('email')
+         ->setCredentialColumn('password');
+         
+
+      $email = $this->_getParam('email');
+      $password = $this->_getParam('password');
+         
+      // Set the input credential values (e.g., from a login form)
+      $authAdapter
+          ->setIdentity($email)
+          ->setCredential($password);
+
+       // from http://framework.zend.com/manual/en/zend.auth.introduction.html
+       // Attempt authentication, saving the result
+       $result = $auth->authenticate($authAdapter);
+       if (!$result->isValid()) {
+          // Authentication failed; print the reasons why
+          foreach ($result->getMessages() as $message) {
+             echo "$message\n";
+          }
+       } else {
+          $this->_helper->redirector('show');
+          // Authentication succeeded; the identity ($username) is stored
+          // in the session
+          // $result->getIdentity() === $auth->getIdentity()
+          // $result->getIdentity() === $username
+       }
+
+/*      
+       // from http://framework.zend.com/manual/en/zend.auth.introduction.html
+       // inside of AuthController / loginAction
+       $result = $this->_auth->authenticate($adapter);
+       switch ($result->getCode()) {
+          case Zend_Auth_Result::FAILURE_IDENTITY_NOT_FOUND:
+          // do stuff for nonexistent identity
+          break;
+          case Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID:
+          // do stuff for invalid credential
+          break;
+          case Zend_Auth_Result::SUCCESS:
+          // do stuff for successful authentication
+          break;
+          default:
+          // do stuff for other failure
+          break;
+       }
+*/  
+
+/*	   
+		if(!isset($userNamespace) && $this->_getParam('login'))
 		{
 			//Implement login action here
 			$this->_helper->redirector('show');
 		}
-		elseif(isset($userNamespace) && $this->_postParam('logout')) {}
+		elseif(isset($userNamespace) && $this->_getParam('logout')) {}
 			//Implement logout action here
 		$this->_helper->redirector('index');
+*/
+
 	}
 	
 	public function updateAction()
